bef219f4f0d5f4c42f067de439f55c86ebe19fba
morti132 <marcin.migdal1996@gmail.com>
2021-04-11 13:57:16 +0200

    added task container

--- src/App.js
+++ src/App.js
@@ -1,9 +1,57 @@
+import React, { useEffect, useState } from 'react'
 import './App.css';
 
 function App() {
+  const [toDoList, setToDoList] = useState(['e','e']);
+  const [taskCountText, setTaskCountText] = useState();
+
+  useEffect(() => {
+    const checkToDolist = () => {
+      if (toDoList.length > 0) {
+        setTaskCountText(
+          <p className="taskCountText">
+            You have {toDoList.length} {toDoList.length > 1 ? 'tasks' : 'task'} left.
+          </p>
+        )
+        return;
+      }
+      setTaskCountText(
+        <p className="taskCountText">
+          Your to do list is empty,
+         <br />
+          add new task to change that!
+      </p>
+      )
+    }
+
+    checkToDolist()
+  }, []);
+
+  const TaskList = () => {
+    const taskList = []
+
+    toDoList.map((task, index) => {
+      taskList.push(
+        <div key={index} className="addTaskBox">
+          {task}
+        </div>
+      )
+    })
+
+    return taskList
+  }
+
   return (
     <div className="container">
-      <p>To do list</p>
+      <p className="title">Forgetto</p>
+      <p className="description">
+        Just save your tasks to <br className='mobileNewLine' /> NOT forget them!
+      </p>
+      <div className="taskContainer">
+        <div className="addTaskBox">+</div>
+        <TaskList />
+      </div>
+      {taskCountText}
     </div>
   );
 }
